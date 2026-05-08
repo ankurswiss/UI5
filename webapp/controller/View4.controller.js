6a58ca276214e2f691361df6a5999b1031375c66
Ankur Pandey <ankur.pandey@jetaviation.com>
2026-05-08 22:59:40 +0000

    DemoStep1

--- webapp/controller/View4.controller.js
+++ webapp/controller/View4.controller.js
@@ -7,19 +7,51 @@ sap.ui.define([
     return Controller.extend("project1.controller.View4", {
         onInit() {
             this.getOwnerComponent().getRouter().getRoute("RouteView4").attachPatternMatched(this.onPatternMatched, this);
-             var oModel = new JSONModel({
+            var oModel = new JSONModel({
                 employees: []
             });
             this.getView().setModel(oModel);
         },
 
-        OnBulkCreate(){
+        OnBulkCreate() {
+            var oModelV4 = this.getView().getModel();
+            var aEmployee = oModelV4.getData().employees;
+            aEmployee.forEach(function (item) {
+                item.StdntId = Number(item.StdntId);
+
+            });
+            var oModel = this.getOwnerComponent().getModel("oModel");
+            var aDefGroup = oModel.getDeferredGroups();
+            aDefGroup = aDefGroup.concat(["CREATEBATCH"]);
+            oModel.setDeferredGroups(aDefGroup);
+
             
+            for (let i = 0; i < aEmployee.length; i++) {
+                oModel.create("/StdntInfoSet", aEmployee[i],
+                    { groupId: "CREATEBATCH" }
+                );
+
+            }
+
+            oModel.submitChanges({
+                groupId: "CREATEBATCH",
+                success: function (req, res) {
+                    if (res.statusCode === "202") {
+                        sap.m.MessageToast.show("Details Updated");
+                    }
+                },
+                error: function (oError) {
+                    sap.m.MessageToast.show(JSON.parse(oError.responseText).error.message.value);
+                }
+            })
+
+        },
+        onBack(){
+            this.getOwnerComponent().getRouter().navTo("RouteView1");
         },
 
-
-        onFileChange(oEvent){
-             var file = oEvent.getParameter("files")[0];
+        onFileChange(oEvent) {
+            var file = oEvent.getParameter("files")[0];
 
             if (!file) {
                 return;
@@ -40,30 +72,31 @@ sap.ui.define([
 
                 // Convert to JSON
                 var jsonData = XLSX.utils.sheet_to_json(sheet);
-                 // Expected format: [{id:1, name:"ABC"}]
+                // Expected format: [{id:1, name:"ABC"}]
                 var employees = jsonData.map(function (row) {
                     return {
-                        id: row.id,
-                        name: row.name
+                        StdntId: row.id,
+                        StdntName: row.name
                     };
                 });
 
                 // Set to model
                 var oModel = this.getView().getModel();
                 oModel.setProperty("/employees", employees);
-               
-
-
-                }.bind(this);
-                 reader.readAsBinaryString(file);
 
 
 
-            
-        }});
+            }.bind(this);
+            reader.readAsBinaryString(file);
 
 
 
 
-           
+        }
+    });
+
+
+
+
+
 });
